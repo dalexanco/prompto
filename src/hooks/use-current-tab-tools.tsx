2a872484e89b4bcd4c661897ce8b66764d3e5665
David-Alexandre COHEN <davidalexandre.cohen@cdiscount.com>
2023-04-18 00:48:44 +0200

    feat: smart pin/unpin depending on active tab

--- src/hooks/use-current-tab-tools.tsx
+++ src/hooks/use-current-tab-tools.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
     PromptCommand,
     PromptCommandType,
@@ -20,12 +21,25 @@ const COMMANDS = [
 ] as PromptCommand[];
 
 export default function useTabTools(input: string): PromptQuery {
+    const [ignoredTypes, ignoreTypes] = useState<PromptCommandType[]>([]);
+    useEffect(() => {
+        (async function () {
+            const [currentTab] = await chrome.tabs.query({
+                lastFocusedWindow: true,
+                active: true,
+            });
+            currentTab?.pinned
+                ? ignoreTypes([PromptCommandType.CURRENT_TAB_PIN])
+                : ignoreTypes([PromptCommandType.CURRENT_TAB_UNPIN]);
+        })();
+    }, []);
+
     const isLoading = false;
     if (!input) return { results: [], isLoading };
 
     const results = COMMANDS.filter(({ title }) =>
         title.toLowerCase().includes(input.toLowerCase()),
-    );
+    ).filter(({ type }) => !ignoredTypes.includes(type));
 
     return { results, isLoading };
 }
