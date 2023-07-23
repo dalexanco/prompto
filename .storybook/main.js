5d02cce84556df1270a7236bb42f984003841203
David-Alexandre COHEN <david.alex.cohen@gmail.com>
2023-07-23 15:48:40 +0200

    feat: integration of storybook (with vite + tailwind)

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  core: {
-    builder: '@storybook/builder-vite' // 👈 The builder enabled here.
-  }
-};
