3bb9854e738be998b77bbf77c5bfa56c64c656b0
David-Alexandre COHEN <david.alex.cohen@gmail.com>
2023-09-10 23:54:37 +0200

    feat: implements react-router

--- src/pages/PopupDetailsPage.tsx
+++ src/pages/PopupDetailsPage.tsx
@@ -1,17 +1,21 @@
-import { CommandSuggestion, CommandType } from '@src/types/commands';
-import { Suggestion } from '../../components/Suggestion/component';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useKeyPressEvent } from 'react-use';
+
+import { CommandType } from '@src/types/commands';
+import { Suggestion } from '../components/Suggestion/component';
 import CommandDetailsSave from '@src/components/CommandDetailsSave';
 import Header from '@src/components/Header';
 import Card from '@src/components/Card';
 import Footer from '@src/components/Footer';
+import { useSuggestionsStore } from '@src/stores';
 
-interface SuggestionDetailsProps {
-  suggestion?: CommandSuggestion;
-}
+export default function PopupDetailsPage() {
+  const { suggestions } = useSuggestionsStore();
+  const { suggestionKey } = useParams();
+  const navigate = useNavigate();
+  useKeyPressEvent('ArrowLeft', () => navigate(-1));
 
-export default function PopupCommandDetails({
-  suggestion
-}: SuggestionDetailsProps) {
+  const suggestion = suggestions.find(({ key }) => key === suggestionKey);
   if (!suggestion) return null;
 
   switch (suggestion.type) {
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-import { PopupPrompt } from './pages/PopupPrompt';
+import PopupHomePage from './pages/PopupHomePage';
+import PopupDetailsPage from './pages/PopupDetailsPage';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <PopupHomePage />
+  },
+  {
+    path: '/details/:suggestionKey',
+    element: <PopupDetailsPage />
+  }
+]);
 
 const container = document.getElementById('popup');
 const root = createRoot(container!);
-root.render(<PopupPrompt />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
